9ecfc02e599d5f7aabf142bd1ab21bc8527b941d
Neil McLaren <neil.mclaren@consensys.net>
2019-07-11 12:04:41 +0100

    Update test for BigNumber changes, and enable skipped tests now that gas cost is lower.
    Add bitmask test (I merged Greg's PRs in the wrong order).
    
    Add more tests, just beacuse I wrote them when tracking down a bug I introduced.

--- test/bitmask.js
+++ test/bitmask.js
@@ -8,32 +8,62 @@ contract('BitmaskMock', () => {
   })
 
   it('correctly identifiers a set bit', async () => {
-    await bitmaskMock.setBit(5) 
+    await bitmaskMock.setBit(5)
     let result = await bitmaskMock.hasBit(5)
     assert(result, 'bitmaskMock should have bit set')
   })
 
   it('correctly identifies an unset bit', async () => {
-    await bitmaskMock.setBit(5) 
+    await bitmaskMock.setBit(5)
     let result = await bitmaskMock.hasBit(6)
     assert(!result, 'bitmaskMock should not have bit set')
   })
 
+  it('correctly identifies an unset high bit', async () => {
+    await bitmaskMock.setBit(5)
+    let result = await bitmaskMock.hasBit(255)
+    assert(!result, 'bitmaskMock should not have high bit set')
+  })
+
   it('allows you to set a bit', async () => {
-    await bitmaskMock.setBit(5) 
+    await bitmaskMock.setBit(5)
     await bitmaskMock.setBit(6)
-    let result = await bitmaskMock.bitmask();
+    let result = await bitmaskMock.bits()
     let expected = (1 << 5) + (1 << 6)
     assert.equal(result, expected, 'should set the bit')
   })
 
   it('allows you to unset a bit', async () => {
-    await bitmaskMock.setBit(5) 
-    await bitmaskMock.setBit(6) 
+    await bitmaskMock.setBit(5)
+    await bitmaskMock.setBit(6)
     await bitmaskMock.unsetBit(6)
     let expected = 1 << 5
-    
-    let result = await bitmaskMock.bitmask();
+
+    let result = await bitmaskMock.bits()
+    assert.equal(result, expected, 'should unset the bit')
+  })
+
+  it('allows you to set a high and low bits', async () => {
+    await bitmaskMock.setBit(0)
+    await bitmaskMock.setBit(255)
+    let result = await bitmaskMock.bits()
+    let expected =
+      '57896044618658097711785492504343953926634992332820282019728792003956564819969'
+    assert.equal(result.toString(), expected, 'should set the bits')
+
+    let hasHighBit = await bitmaskMock.hasBit(255)
+    let hasLowBit = await bitmaskMock.hasBit(0)
+    assert.equal(hasHighBit, true, 'high bit is set')
+    assert.equal(hasLowBit, true, 'low bit is set')
+  })
+
+  it('allows you to unset high and low bits', async () => {
+    await bitmaskMock.setBit(0)
+    await bitmaskMock.setBit(255)
+    await bitmaskMock.unsetBit(255)
+    let expected = 1
+
+    let result = await bitmaskMock.bits()
     assert.equal(result, expected, 'should unset the bit')
   })
 })
